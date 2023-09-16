b5672964c1c36db369018c5f165fde2e63d27c3b
Penguinho <letitrainplz@naver.com>
2023-09-16 20:32:57 +0900

    refacotring

--- main.py
+++ main.py
@@ -7,58 +7,66 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-KEYWORD = "buy domain"
-CLASSNAME = "asEBEc"
 
+# * Chrome Sets
 chrome_options = Options()
+service = Service(executable_path=ChromeDriverManager().install())
 chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
 chrome_options.add_experimental_option("detach", True)
 
-service = Service(executable_path=ChromeDriverManager().install())
-browser = webdriver.Chrome(service=service, options=chrome_options)
-browser.get("https://google.com")
-browser.maximize_window()
-search_bar = browser.find_element(By.NAME, "q")
-search_bar.send_keys(KEYWORD)
-search_bar.send_keys(Keys.ENTER)
 
-# search_results = browser.find_elements(By.CLASS_NAME, "yuRUbf")
-# print(search_results)
+class GoogleKeywordScreenshooter:
+    def __init__(self, keyword, screenshots_dir, class_name):
+        self.keyword = keyword
+        self.class_name = class_name
+        self.screenshots_dir = screenshots_dir
+        self.browser = webdriver.Chrome(service=service, options=chrome_options)
 
-try:
-    # * 20s(페이지를 다 로딩할 때까지의 시간이 필요하다.) => 그래야 class_name을 찾는다. ✅
-    search_results = WebDriverWait(browser, 20).until(
-        EC.presence_of_all_elements_located((By.CLASS_NAME, CLASSNAME))
-    )
-    print(f"총 결과값은 {len(search_results)}개 입니다.")
-except Exception:
-    print("불러오지 못했습니다.")
+    def start(self):
+        self.browser.get("https://google.com")
+        # self.browser.maximize_window()
+        search_bar = self.browser.find_element(By.NAME, "q")  # google search input name
+        search_bar.send_keys(self.keyword)
+        search_bar.send_keys(Keys.ENTER)
 
-# search_results.pop()
+        try:
+            shitty_element = WebDriverWait(self.browser, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "cUnQKe"))
+            )
+            print(shitty_element)
+            self.browser.execute_script(
+                """
+                const shitty = arguments[0];
+                shitty.parentElement.removeChild(shitty);
+            """,
+                shitty_element,
+            )
+        except Exception:
+            pass
 
-shitty_element = WebDriverWait(browser, 20).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "cUnQKe"))
-)
-print(shitty_element)
+            # 20s(페이지를 다 로딩할 때까지의 시간이 필요하다.) => 그래야 class_name을 찾는다. ✅
+        search_results = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_all_elements_located((By.CLASS_NAME, self.class_name))
+        )
+        print(f"총 결과값은 {len(search_results)}개 입니다.")
 
-browser.execute_script(
-    """
-    const shitty = arguments[0];
-    shitty.parentElement.removeChild(shitty);
-""",
-    shitty_element,
-)
+        for index, search_result in enumerate(search_results):
+            search_result.screenshot(
+                f"{self.screenshots_dir}/{self.keyword}x{index}.png"
+            )
+            print(f"{self.keyword}{index} download complete!")
+            # title = WebDriverWait(search_result, 20).until(
+            #     EC.presence_of_element_located((By.TAG_NAME, "h3"))
+            # )
+            # print(title.text)
 
-# * "MjjYud" class 내에서 <h3> 추출
-for index, search_result in enumerate(search_results):
-    try:
-        search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
-        # title = WebDriverWait(search_result, 20).until(
-        #     EC.presence_of_element_located((By.TAG_NAME, "h3"))
-        # )
-        # print(title.text)
-    except Exception:
-        print("불러오지 못했습니다.")
+    def finish(self):
+        self.browser.quit()
+
+        # search_results = browser.find_elements(By.CLASS_NAME, "yuRUbf")
+        # print(search_results)
 
 
-browser.quit()
+flutter_search = GoogleKeywordScreenshooter("flutter", "screenshots", "asEBEc")
+flutter_search.start()
+flutter_search.finish()
